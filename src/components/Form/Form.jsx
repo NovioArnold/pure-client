299b8a7ef460703fb6beabdd3150ba3b5aed4e80
Arnold Hilberink <104128195+NovioArnold@users.noreply.github.com>
2023-03-19 22:37:16 +0100

    reducer added v2

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,12 @@
+import useStyles from "./styles";
+
+const Form = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <h1>Create a Memory</h1>
+    </>
+  );
+};
+
+export default Form;
---
+++ src/components/Posts/post/Post.jsx
@@ -0,0 +1,11 @@
+import useStyles from "./styles";
+const Post = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <h1># not a real Post yet </h1>
+    </>
+  );
+};
+
+export default Post;
