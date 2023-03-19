19fecf9ddc88ebc20a1546322ae80cad5ed203c6
Arnold Hilberink <104128195+NovioArnold@users.noreply.github.com>
2023-03-19 20:55:43 +0100

    stylesheets added for Posts,Post and Form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import ReactDOM, { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './app'
+import App from "./app";
 
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
-
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
